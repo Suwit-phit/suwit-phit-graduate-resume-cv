ade43a7bbecf87542f8de2f2b22a2f6996d09f3c
Saw Tar So Gay Htoo Htoo <6431503130@lamduan.mfu.ac.th>
2024-10-31 16:41:51 +0700

    update cover letter url

--- src/app/page.js
+++ src/app/page.js
@@ -136,7 +136,8 @@ export default function Home() {
           <p>Please click the link below</p>
           <p>to see the Cover Letter</p>
           <a className="underline text-blue-500 hover:text-blue-300" target='blank'
-            href="https://docs.google.com/document/d/160Zd19GmuUn4um8VWZ8XTJYGrEDsKVwkBbmDXsQGBbs/edit?usp=sharing"
+            // href="https://docs.google.com/document/d/160Zd19GmuUn4um8VWZ8XTJYGrEDsKVwkBbmDXsQGBbs/edit?usp=sharing" //! KTC
+            href="https://docs.google.com/document/d/1Bw5ZL6-K72zVK475_4HK-7xv2mj9Sp1-FR3GUYzT6sA/edit?usp=sharing" //! Lapaschawal
           >Cover Letter</a>
         </div>
       </div>
